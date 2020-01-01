4ad69e36257c3b9a510c38cbc95d7f4858077e3d
Victorvs1967 <victorsmirnov@me.com>
2020-01-02 01:46:25 +0200

    add messages

--- app/models.py
+++ app/models.py
@@ -71,6 +71,13 @@ class User(UserMixin, db.Model):
                 secondaryjoin=(followers.c.followed_id == id),
                 backref=db.backref('followers', lazy='dynamic'),
                 lazy='dynamic')
+    messages_send = db.relationship('Message', foreign_keys='Message.sender_id', backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message', foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -122,3 +129,13 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return f'Post {self.body}'
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Message {self.body}>'
