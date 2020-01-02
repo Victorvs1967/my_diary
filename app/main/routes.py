043de1881e4465273d540de82f03d18de99ca4ed
Victorvs1967 <victorsmirnov@me.com>
2020-01-02 12:50:05 +0200

    add notifications

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,7 +5,7 @@ from flask_babel import get_locale, _
 from guess_language import guess_language
 from werkzeug.urls import url_parse
 from app import db
-from app.models import User, Post, Message
+from app.models import User, Post, Message, Notification
 from app.translate import translate
 from app.main import bp, routes
 from app.main.forms import EditProfileForm, PostForm, SearchForm, MessageForm
@@ -135,6 +135,7 @@ def send_message(recipient):
     if form.validate_on_submit():
         msg = Message(author=current_user, recipient=user, body=form.message.data)
         db.session.add(msg)
+        user.add_notification('unread_messages_count', user.new_messages())
         db.session.commit()
         flash(_('Your message has been sent.'))
         return redirect(url_for('main.user', username=recipient))
@@ -144,6 +145,7 @@ def send_message(recipient):
 @login_required
 def messages():
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
@@ -151,3 +153,10 @@ def messages():
     next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
     prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None
     return render_template('messages.html', messages=messages.items, next_url=next_url, prev_url=prev_url)
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{'name': n.name, 'data': n.get_data(), 'timestamp': n.timestamp} for n in notifications])
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import json
 from time import time
 from datetime import datetime
 import jwt
@@ -74,6 +75,7 @@ class User(UserMixin, db.Model):
     messages_send = db.relationship('Message', foreign_keys='Message.sender_id', backref='author', lazy='dynamic')
     messages_received = db.relationship('Message', foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 
     def new_messages(self):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
@@ -111,6 +113,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expiers_in=600):
         return jwt.encode({'reset_password': self.id, 'exp': time() + expiers_in}, current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+ 
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -139,3 +147,13 @@ class Message(db.Model):
 
     def __repr__(self):
         return f'<Message {self.body}>'
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
