523141c89a17a0735b10167007e889fb37080f4d
Victor Smirnov <victors@Victors-MacBook-Pro.local>
2020-01-05 13:26:50 +0200

    remove reindex

--- app/models.py
+++ app/models.py
@@ -23,7 +23,6 @@ class SearchableMixin(object):
 
     @classmethod
     def before_commit(cls, session):
-        Post.reindex()
         session._changes = {
             'add': list(session.new),
             'update': list(session.dirty),
